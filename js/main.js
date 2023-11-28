16e6a7836fdbed519e1a5c65343122da639ae7c9
ibportugal <152249517+ibportugal@users.noreply.github.com>
2023-11-28 15:24:37 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["MMS142 Group K 2023", "Designer.", "Developer.", "Freelancer.", "Photographer"],
       typeSpeed: 100,
       loop: true,
     });
